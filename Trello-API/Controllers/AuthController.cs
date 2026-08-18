ff57d3ad9fa756a0c96f89e3b61b36253a960895
dinh0107 <duongvandinh.work@gmail.com>
2026-08-18 17:13:07 +0700

    up

--- Trello-API/Controllers/AuthController.cs
+++ Trello-API/Controllers/AuthController.cs
@@ -145,6 +145,62 @@ namespace Trello_API.Controllers
         }
 
 
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("register")]
+        public IHttpActionResult Register([FromBody] RegisterRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Password))
+            {
+                return Content(HttpStatusCode.BadRequest, new
+                {
+                    Success = false,
+                    Message = "Vui lòng nhập đầy đủ Username và Password"
+                });
+            }
+
+            var exists = _unitOfWork.UserRepository.GetQuery(u => u.Email == request.Email).FirstOrDefault();
+            if (exists != null)
+            {
+                return Content(HttpStatusCode.Conflict, new
+                {
+                    Success = false,
+                    Message = "Tài khoản đã tồn tại"
+                });
+            }
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            var newUser = new User
+            {
+                FullName = request.FullName,
+                Phone = request.Phone,
+                AvatarUrl = "",
+                Email = request.Email,
+                PasswordHash = passwordHash
+            };
+
+            var accessToken = JwtHelper.GenerateJwtToken(newUser.Email, 120);
+            var refreshToken = Guid.NewGuid().ToString();
+            newUser.RefreshToken = refreshToken;
+            newUser.RefreshTokenExpiry = DateTime.UtcNow.AddDays(7);
+            _unitOfWork.UserRepository.Insert(newUser);
+            var cookie = new HttpCookie("AccessToken", accessToken)
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None
+            };
+            HttpContext.Current.Response.Cookies.Add(cookie);
+            return Ok(new
+            {
+                Success = true,
+                Message = "Đăng ký thành công, bạn đã được đăng nhập",
+                RefreshToken = refreshToken,
+                User = new { newUser.Id, newUser.Email }
+            });
+        }
+
+
 
         [AllowAnonymous]
         [HttpGet]
